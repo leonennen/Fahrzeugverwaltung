531d163de3289cbb78125d54590bc667c9398ee0
leon <leon@leon-965P-DS3>
2016-01-08 14:48:13 +0100

    Alle Exceptions fertig

--- src/main/java/de/decoit/fahrzeugverwaltung/methodenKlassen/ExportAuswahl.java
+++ src/main/java/de/decoit/fahrzeugverwaltung/methodenKlassen/ExportAuswahl.java
@@ -1,7 +1,7 @@
 package de.decoit.fahrzeugverwaltung.methodenKlassen;
 
-import de.decoit.fahrzeugverwaltung.interfaceKlassen.ExportInterface;
 import de.decoit.fahrzeugverwaltung.enumKlassen.Datei;
+import de.decoit.fahrzeugverwaltung.interfaceKlassen.ExportInterface;
 import de.decoit.fahrzeugverwaltung.subKlassen.BerichtExport;
 import de.decoit.fahrzeugverwaltung.subKlassen.CsvExport;
 import de.decoit.fahrzeugverwaltung.subKlassen.XmlExport;
@@ -9,19 +9,25 @@ import de.decoit.fahrzeugverwaltung.subKlassen.XmlExport;
 public class ExportAuswahl {
 
     public static ExportInterface auswahl(Datei datei) {
+
         ExportInterface export = null;
-        switch (datei) {
-            case XML:
-                export = new XmlExport();
-                break;
-            case Bericht:
-                export = new BerichtExport();
-                break;
-            case CSV:
-                export = new CsvExport();
-                break;
-            default:
-                new IllegalStateException("Undefiniert!");
+        try {
+            switch (datei) {
+                case XML:
+                    export = new XmlExport();
+                    break;
+                case Bericht:
+                    export = new BerichtExport();
+                    break;
+                case CSV:
+                    export = new CsvExport();
+                    break;
+                default:
+                    throw new IllegalStateException("Undefiniert!");
+            }
+        } catch (IllegalStateException ex) {
+
+            System.out.println(ex.getMessage());
         }
         return export;
     }
--- src/main/java/de/decoit/fahrzeugverwaltung/methodenKlassen/Verschleißwerte.java
+++ src/main/java/de/decoit/fahrzeugverwaltung/methodenKlassen/Verschleißwerte.java
@@ -1,7 +1,7 @@
 package de.decoit.fahrzeugverwaltung.methodenKlassen;
 
-import de.decoit.fahrzeugverwaltung.enumKlassen.Klasse;
 import de.decoit.fahrzeugverwaltung.KFZ;
+import de.decoit.fahrzeugverwaltung.enumKlassen.Klasse;
 
 public class Verschleißwerte {
 
@@ -11,43 +11,49 @@ public class Verschleißwerte {
         double verschleißwert = 0;
         double streckenkorrektur = 1 + (auto.getKmstand() / 500000);
 
-        switch (klasse) {
-            case Kleinwagen:
-                verschleißwert = strecke - 20;
-                if (strecke < 40) {
-                    verschleißwert = strecke * 0.2;
-                }
-                break;
-            case Mittelklasse:
-                verschleißwert = strecke * 0.6;
-                if (strecke < 50) {
-                    verschleißwert = 25;
-                } else if (strecke > 500) {
-                    verschleißwert = strecke * 0.8;
-                }
-                break;
-            case Oberklasse:
-                verschleißwert = strecke * 0.3 + 100;
-                if (strecke < 500) {
+        try {
+            switch (klasse) {
+                case Kleinwagen:
+                    verschleißwert = strecke - 20;
+                    if (strecke < 40) {
+                        verschleißwert = strecke * 0.2;
+                    }
+                    break;
+                case Mittelklasse:
+                    verschleißwert = strecke * 0.6;
+                    if (strecke < 50) {
+                        verschleißwert = 25;
+                    } else if (strecke > 500) {
+                        verschleißwert = strecke * 0.8;
+                    }
+                    break;
+                case Oberklasse:
+                    verschleißwert = strecke * 0.3 + 100;
+                    if (strecke < 500) {
+                        verschleißwert = strecke * 0.4;
+                    } else if (strecke > 1000) {
+                        verschleißwert = strecke * 0.2;
+                    }
+                    break;
+                case Transporter:
                     verschleißwert = strecke * 0.4;
-                } else if (strecke > 1000) {
-                    verschleißwert = strecke * 0.2;
-                }
-                break;
-            case Transporter:
-                verschleißwert = strecke * 0.4;
-                if (strecke < 100) {
-                    verschleißwert = strecke * 0.4 + 20;
-                } else if (strecke > 300) {
-                    verschleißwert = strecke * 0.3;
-                }
+                    if (strecke < 100) {
+                        verschleißwert = strecke * 0.4 + 20;
+                    } else if (strecke > 300) {
+                        verschleißwert = strecke * 0.3;
+                    }
 
-                break;
-            default:
-                throw new IllegalStateException("Undefinierte Fahrzeugklasse!");
+                    break;
+                default:
+                    throw new IllegalStateException("Undefinierte Fahrzeugklasse!");
+            }
+        } catch (IllegalStateException ex) {
+
+            System.out.println(ex.getMessage());
         }
         verschleißwert = verschleißwert * streckenkorrektur;
 
         return verschleißwert;
     }
+
 }
