d5857b4139bf9ac71bd0d62bf3c02b59aae8b7e2
leon <leon@leon-965P-DS3>
2016-03-01 10:01:39 +0100

    SQL in Objekt

--- src/main/java/de/decoit/fahrzeugverwaltung/Fahrzeug.java
+++ src/main/java/de/decoit/fahrzeugverwaltung/Fahrzeug.java
@@ -1,16 +1,23 @@
 package de.decoit.fahrzeugverwaltung;
 
 public class Fahrzeug {
+
+    public String besitzer;
+    public String marke;
+    public String typ;
+    public double verbrauch;
+    public int leistung;
+    public int kmstand;
+
+    public Fahrzeug(String besitzer, String marke, String typ, double verbrauch, int leistung, int kmstand) {
+
+        this.besitzer = besitzer;
+        this.marke = marke;
+        this.typ = typ;
+        this.verbrauch = verbrauch;
+        this.leistung = leistung;
+        this.kmstand = kmstand;
+
+    }
+
 }
-
-
-//    public Fahrzeug(String besitzer, String marke, String typ, double verbrauch, int leistung, int kmstand) {
-//
-//        this.besitzer = besitzer;
-//        this.marke = marke;
-//        this.typ = typ;
-//        this.verbrauch = verbrauch;
-//        this.leistung = leistung;
-//        this.kmstand = kmstand;
-//
-//}
